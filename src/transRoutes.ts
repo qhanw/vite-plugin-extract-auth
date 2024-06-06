bae7badec13afe3c715e03107136af06de65d64d
qhanw <whenhan@foxmail.com>
2024-06-06 20:48:23 +0800

    feat: only resolve routes below the root

--- src/transRoutes.ts
+++ src/transRoutes.ts
@@ -58,7 +58,7 @@ export default function transRoutes(cfg: Options) {
   const text = readFileSync(filePath, 'utf8');
   const { code } = esbuild.transformSync(text, { loader: 'ts', minify: true });
   const jsonText = /\[\{.*\}\];/.exec(code)?.[0];
-  const json = new Function('return ' + jsonText)();
+  const json: AuthRoute[] = new Function('return ' + jsonText)();
 
   const parse = (
     routes: AuthRoute[],
@@ -79,82 +79,77 @@ export default function transRoutes(cfg: Options) {
       // 以点分隔的全路径文件名及完整路由路径
       const full = {
         name: pName ? [pName, name].join('.') : name,
-        path: pPath && path.startsWith('/') ? path : [pPath, path].join('/'),
+        path: path.startsWith('/')
+          ? path
+          : '/' +
+            [...(pPath?.split('/') || []), ...(path?.split('/') || [])]
+              .filter(Boolean)
+              .join('/'),
       };
 
-      // 忽略path=/第一级菜单
-      if (path !== '/') {
-        // 对于相同路径菜单做特殊处理
-        if (!element && children) {
-          const specialRouteIndex = getSpecifiedRouteIndex(children, curr);
+      // 对于相同路径菜单做特殊处理
+      if (!element && children) {
+        const specialRouteIndex = getSpecifiedRouteIndex(children, curr);
 
-          const specialRoute = children[specialRouteIndex];
+        const specialRoute = children[specialRouteIndex];
 
-          if (specialRoute) {
-            element = specialRoute.element;
-            children.splice(specialRouteIndex, 1);
-          }
+        if (specialRoute) {
+          element = specialRoute.element;
+          children.splice(specialRouteIndex, 1);
         }
+      }
 
-        // 生成权限 KEY 并校验 KEY值是否可用
-        const key = validKey(curr.authKey || path2key(full.path));
+      // 生成权限 KEY 并校验 KEY值是否可用
+      const key = validKey(curr.authKey || path2key(full.path));
 
-        let childAuth: AuthTreeNode[] | undefined;
+      let childAuth: AuthTreeNode[] | undefined;
 
-        // 当路由没有子路由时，获取当前路由对应文件夹下的权限文件，并提取权限内容绑定到权限资源树上。
-        // 当子路由时存在时，继续遍历路由并生成路由权限
-        if (children?.length) {
-          childAuth = parse(children, {
-            pName: full.name,
-            pPath: full.path,
-          });
-        } else {
-          const currPath = join(cfg.cwd, 'src/pages/', element!);
-          const { fileDir } = getFileNameOrFileDir(currPath);
-
-          // 使用单个的权限配置文件
-          const content = readFileContent(join(fileDir, 'auth.json'));
-          const apiPrefix = cfg.apiPrefix;
-
-          if (content) {
-            childAuth = trans({ data: content, apiPrefix });
-          } else {
-            const collection = scanDir(join(fileDir, 'auth'))?.map(
-              (filePath) => {
-                const fc = readFileContent(filePath);
-                return trans({ data: fc, apiPrefix });
-              },
-            );
-
-            childAuth = collection?.flat(1);
-          }
-        }
-
-        // 将配置文件中的权限API配置与路由页面进行合并
-        const { path: p, json } = combinePaths(childAuth, key, path);
-
-        const authTree: AuthTreeNode = {
-          name,
-          key,
-          type: 'route',
-          path: p,
-          hidden: curr.hideInMenu,
-          icon: curr.icon,
-          children: json,
-        };
-
-        prev.push(authTree);
-      } else {
-        const res = parse(children!, {
+      // 当路由没有子路由时，获取当前路由对应文件夹下的权限文件，并提取权限内容绑定到权限资源树上。
+      // 当子路由时存在时，继续遍历路由并生成路由权限
+      if (children?.length) {
+        childAuth = parse(children, {
           pName: full.name,
           pPath: full.path,
         });
-        prev = [...prev, ...res];
+      } else {
+        const currPath = join(cfg.cwd, 'src/pages/', element!);
+        const { fileDir } = getFileNameOrFileDir(currPath);
+
+        // 使用单个的权限配置文件
+        const content = readFileContent(join(fileDir, 'auth.json'));
+        const apiPrefix = cfg.apiPrefix;
+
+        if (content) {
+          childAuth = trans({ data: content, apiPrefix });
+        } else {
+          const collection = scanDir(join(fileDir, 'auth'))?.map((filePath) => {
+            const fc = readFileContent(filePath);
+            return trans({ data: fc, apiPrefix });
+          });
+
+          childAuth = collection?.flat(1);
+        }
       }
+
+      // 将配置文件中的权限API配置与路由页面进行合并
+      const { path: p, json } = combinePaths(childAuth, key, full.path);
+
+      const authTree: AuthTreeNode = {
+        name,
+        key,
+        type: 'route',
+        path: p,
+        hidden: curr.hideInMenu,
+        icon: curr.icon,
+        children: json,
+      };
+
+      prev.push(authTree);
+
       return prev;
     }, [] as AuthTreeNode[]);
   };
 
-  // return [];
-  return parse(json);
+  // 只有根下的路由具有权限配置
+  return parse(json.find((c) => c.path === '/')?.children || []);
 }
